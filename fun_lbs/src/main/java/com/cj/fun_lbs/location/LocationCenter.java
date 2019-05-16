7f916c3c51d7e9fb37c66a397630288c14f880e3
chris-jason1224 <chrisjason1224@gmail.com>
2019-05-16 17:21:39 +0800

    Update fun_lbs/src/main/java/com/cj/fun_lbs/location/LocationCenter.java

--- fun_lbs/src/main/java/com/cj/fun_lbs/location/LocationCenter.java
+++ fun_lbs/src/main/java/com/cj/fun_lbs/location/LocationCenter.java
@@ -23,7 +23,7 @@ import java.util.List;
  * Package:com.cj.fun_lbs.location
  * 处理定位逻辑的工具类
  */
-public class LocationCenter{
+public class LocationCenter {
 
     private static Context context;
     private AMapLocationClient mLocationClient;
@@ -82,44 +82,53 @@ public class LocationCenter{
     private AMapLocationListener aMapLocationListener = new AMapLocationListener() {
         @Override
         public void onLocationChanged(AMapLocation aMapLocation) {
-            if (aMapLocation != null) {
-                if (aMapLocation.getErrorCode() == 0) {
 
-                    LocationInfoEntity entity = new LocationInfoEntity();
-                    entity.setLocationType(aMapLocation.getLocationType());
-                    entity.setLatitude(aMapLocation.getLatitude());
-                    entity.setLongitude(aMapLocation.getLongitude());
-                    entity.setAccuracy(aMapLocation.getAccuracy());
-                    entity.setAddress(aMapLocation.getAddress());
-                    entity.setCountry(aMapLocation.getCountry());
-                    entity.setProvince(aMapLocation.getProvince());
-                    entity.setCity(aMapLocation.getCity());
-                    entity.setDistrict(aMapLocation.getDistrict());
-                    entity.setStreet(aMapLocation.getStreet());
-                    entity.setStreetNum(aMapLocation.getStreetNum());//街道门牌号信息
-                    entity.setCityCode(aMapLocation.getCityCode());//城市编码
-                    entity.setAdCode(aMapLocation.getAdCode());//地区编码
-                    entity.setAoiName(aMapLocation.getAoiName());//获取当前定位点的AOI信息
-                    entity.setBuildingId(aMapLocation.getBuildingId());//获取当前室内定位的建筑物Id
-                    entity.setFloor(aMapLocation.getFloor());//获取当前室内定位的楼层
-                    entity.setGpsAccuracyStatus(aMapLocation.getGpsAccuracyStatus());//获取GPS的当前状态
-                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//获取定位时间
-                    Date date = new Date(aMapLocation.getTime());
-                    entity.setDate(df.format(date));
-
-                    for (ILocateResultCallback callback:callbackList){
-                        callback.onLocation(entity);
-                    }
-
-                } else {
-                    //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
-                    CJLog.getInstance().log_e("定位错误码：" + aMapLocation.getErrorCode());
-                    CJLog.getInstance().log_e("定位错误信息：" + aMapLocation.getErrorInfo());
-                }
+            if (aMapLocation == null) {
+                dispatchLocationInfo(null);
+                return;
             }
+
+            if (aMapLocation.getErrorCode() != 0) {
+                dispatchLocationInfo(null);
+                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                CJLog.getInstance().log_e("定位错误码：" + aMapLocation.getErrorCode());
+                CJLog.getInstance().log_e("定位错误信息：" + aMapLocation.getErrorInfo());
+                return;
+            }
+
+            /**定位成功**/
+            LocationInfoEntity entity = new LocationInfoEntity();
+            entity.setLocationType(aMapLocation.getLocationType());
+            entity.setLatitude(aMapLocation.getLatitude());
+            entity.setLongitude(aMapLocation.getLongitude());
+            entity.setAccuracy(aMapLocation.getAccuracy());
+            entity.setAddress(aMapLocation.getAddress());
+            entity.setCountry(aMapLocation.getCountry());
+            entity.setProvince(aMapLocation.getProvince());
+            entity.setCity(aMapLocation.getCity());
+            entity.setDistrict(aMapLocation.getDistrict());
+            entity.setStreet(aMapLocation.getStreet());
+            entity.setStreetNum(aMapLocation.getStreetNum());//街道门牌号信息
+            entity.setCityCode(aMapLocation.getCityCode());//城市编码
+            entity.setAdCode(aMapLocation.getAdCode());//地区编码
+            entity.setAoiName(aMapLocation.getAoiName());//获取当前定位点的AOI信息
+            entity.setBuildingId(aMapLocation.getBuildingId());//获取当前室内定位的建筑物Id
+            entity.setFloor(aMapLocation.getFloor());//获取当前室内定位的楼层
+            entity.setGpsAccuracyStatus(aMapLocation.getGpsAccuracyStatus());//获取GPS的当前状态
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//获取定位时间
+            Date date = new Date(aMapLocation.getTime());
+            entity.setDate(df.format(date));
+
+            dispatchLocationInfo(entity);
+
         }
     };
 
+    private void dispatchLocationInfo(LocationInfoEntity entity) {
+        for (ILocateResultCallback callback : callbackList) {
+            callback.onLocation(entity);
+        }
+    }
 
 
     //开始定位
@@ -132,7 +141,7 @@ public class LocationCenter{
 
     //停止定位
     public void stopLocate() {
-        if(mLocationClient.isStarted()){
+        if (mLocationClient.isStarted()) {
             mLocationClient.stopLocation();
         }
     }
